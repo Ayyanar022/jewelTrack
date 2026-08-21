14d9246457483096c836543d4060fcc5ad3e9114
Ayyanar <ayan.prof022@example.com>
2026-08-21 23:35:20 +0530

    fix:error fix in subscription service

--- jeweltrack-api/src/subscription/subscription.module.ts
+++ jeweltrack-api/src/subscription/subscription.module.ts
@@ -5,7 +5,8 @@ import { SubscriptionService } from "./subscription.service";
 
 @Module({
     controllers:[SubscriptionController],
-    providers:[SubscriptionService]
+    providers:[SubscriptionService],
+    exports:[SubscriptionService]
 })
 
 export class SubscriptionModule {}
